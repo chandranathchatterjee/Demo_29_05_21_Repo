5434bbb8c3021e85065a53fce2f0f4819083406b
Chandranath Chatterjee <chandranathchatterjee586@gmail.com>
2021-05-29 23:51:59 +0530

    Update TC_TestNG_Practice.java

--- src/test/java/com/inetBanking/testCases/TC_TestNG_Practice.java
+++ src/test/java/com/inetBanking/testCases/TC_TestNG_Practice.java
@@ -4,6 +4,7 @@ import org.testng.annotations.Test;
 
 public class TC_TestNG_Practice {
 
+// this is a sample practice testNG code
     @Test
     public void testCase_1(){
         System.out.println("I am under test case 1");

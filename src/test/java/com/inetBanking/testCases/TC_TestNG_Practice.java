5c8343c9b1050d5f5776c2c8e40cf2d6f717eae5
Chandranath <chandranathchatterjee586@gmail.com>
2021-05-29 23:32:13 +0530

    testNG parctice test cases have been added

---
+++ src/test/java/com/inetBanking/testCases/TC_TestNG_Practice.java
@@ -0,0 +1,16 @@
+package com.inetBanking.testCases;
+
+import org.testng.annotations.Test;
+
+public class TC_TestNG_Practice {
+
+    @Test
+    public void testCase_1(){
+        System.out.println("I am under test case 1");
+    }
+
+    @Test
+    public void testCase_2(){
+        System.out.println("I am under test case 2");
+    }
+}
